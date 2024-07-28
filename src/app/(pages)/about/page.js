9607a26c2b54d531f9de2a571665600e31f79cbc
devp1866 <devp1866@gmail.com>
2024-07-28 13:49:09 +0530

    :zap: vercel

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -1,5 +1,6 @@
 import Foot from "@/components/Foot"
 import Nav from "@/components/Nav"
+import Image from "next/image"
 
 const links = [
     { name: 'Open roles', href: '#' },
@@ -18,8 +19,10 @@ const links = [
     return (
         <>
       <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-        <img
-          src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+        <Image
+        width={1080}
+        height={1080}
+          src="/about.jpg"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
         />
